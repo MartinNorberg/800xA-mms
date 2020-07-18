97b8bc8382258a8c1cfa458f3c1d68641cc6e800
MartinNorberg <martin.norberg@gautomation.se>
2020-07-18 18:03:49 +0200

    Initial C# commit

---
+++ .Net/800xAmms/MMSComunication.test/ClientTest.cs
@@ -0,0 +1,22 @@
+﻿namespace MMSComunication.test
+{
+    using System;
+    using MMSComunication;
+    using NUnit.Framework;
+
+    public class ClientTest
+    {
+        [Test]
+        public void ClientShouldThrowArgumentExceptionIfEmptyIpAddress()
+        {
+            Assert.Throws<ArgumentException>(() => new Client("",102));
+        }
+
+        [Test]
+        public void PortShouldBeDefault102()
+        {
+            var client = new Client("127.0.0.1");
+            Assert.AreEqual(client.Port, 102);
+        }
+    }
+}
---
+++ .Net/800xAmms/MMSComunication/Client.cs
@@ -0,0 +1,25 @@
+﻿namespace MMSComunication
+{
+    using System;
+
+    public class Client : IClient
+    {
+        private readonly string ipAddress;
+        private readonly int port;
+
+        public Client(string ipAddress, int port = 102)
+        {
+            if (string.IsNullOrEmpty(ipAddress))
+            {
+                throw new ArgumentException("IpAddress is empty");
+            }
+
+            this.ipAddress = ipAddress;
+            this.port = port;
+        }
+
+        public int Port => port;
+
+        public string IpAddress => ipAddress;
+    }
+}
